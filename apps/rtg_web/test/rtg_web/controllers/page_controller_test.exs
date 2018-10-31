1f4dab9520bf669fb9853e9c6ab4fddd3c5d6365
ne_Sachirou <utakata.c4se@gmail.com>
2018-10-31 20:07:32 +0900

    Fix: test

--- apps/rtg_web/test/rtg_web/controllers/page_controller_test.exs
+++ apps/rtg_web/test/rtg_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RtgWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Rtg"
   end
 end
